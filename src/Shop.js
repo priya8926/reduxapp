80f3dd5ef99f5100ff436603830bf17707c3beff
Priya <priyapatel.892002@gmail.com>
2023-09-28 16:52:43 +0530

    reduxapp

---
+++ src/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+  return (
+    <div>
+        <h3 className='mt-2'>Buy items</h3>
+      <button className='btn btn-primary my-2 mx-2'>-</button>
+      Add this item to cart
+      <button className='btn btn-primary  my-2 mx-2'>+</button>
+    </div>
+  )
+}
+
+export default Shop
